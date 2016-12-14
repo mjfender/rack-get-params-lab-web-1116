9601e144dd20a920b617cceef49a4919a51c0eb2
Matt Fender <mjfender@gmail.com>
2016-12-14 15:30:48 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
 class Application
 
+  require 'pry'
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,6 +15,16 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      resp.write handle_cart
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      if @@items.include?(item)
+        @@cart << item
+        resp.write "added #{item}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
@@ -27,4 +39,15 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  def handle_cart
+    if @@cart.empty?
+      "Your cart is empty"
+    else
+      @@cart.collect do |item|
+        "#{item}\n"
+      end.join
+    end
+  end
+
 end
